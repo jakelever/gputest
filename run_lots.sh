f6172e588e262dd1da70672a98f6d8e824de85ee
Jake Lever <jake.lever@gmail.com>
2023-12-20 15:43:06 +0000

    Switch to token classification example

--- run_lots.sh
+++ run_lots.sh
@@ -5,7 +5,7 @@ num_epochs=16
 
 for learning_rate in 1e-3 5e-4 1e-4 5e-5 1e-5; do
 	for batch_size in 8 16 32 64; do
-		python run.py --learning_rate $learning_rate --batch_size $batch_size --num_epochs $num_epochs > log.$learning_rate.$batch_size.$num_epochs
+		python run_tokenclassification.py --learning_rate $learning_rate --batch_size $batch_size --num_epochs $num_epochs > log.$learning_rate.$batch_size.$num_epochs
 	done
 done
 
