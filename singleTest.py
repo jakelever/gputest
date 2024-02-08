ccd0e67da3f5745c02d56e45045d188d7b82d942
Jake Lever <jake.lever@gmail.com>
2024-02-08 00:08:42 +0000

    More code trimming

--- singleTest.py
+++ singleTest.py
@@ -7,18 +7,14 @@ from datasets import load_dataset
 def main():
 	set_seed(42)
 	
+	dataset = load_dataset("jakelever/debug_drugprot2")
+	
 	model_name = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
 
 	tokenizer = AutoTokenizer.from_pretrained(model_name)
-	tokenizer.add_tokens(["[E1]", "[/E1]", "[E2]", "[/E2]"])
-
-	dataset = load_dataset("jakelever/debug_drugprot2")
+	model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=13, problem_type="multi_label_classification")
 	
 	data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-	
-	reltypes = ['ACTIVATOR', 'AGONIST', 'AGONIST-ACTIVATOR', 'AGONIST-INHIBITOR', 'ANTAGONIST', 'DIRECT-REGULATOR', 'INDIRECT-DOWNREGULATOR', 'INDIRECT-UPREGULATOR', 'INHIBITOR', 'PART-OF', 'PRODUCT-OF', 'SUBSTRATE', 'SUBSTRATE_PRODUCT-OF']
-
-	model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=len(reltypes), problem_type="multi_label_classification")
 		
 	training_args = TrainingArguments(
 		output_dir = "awesome_model",
@@ -27,25 +23,18 @@ def main():
 		per_device_eval_batch_size=4,
 		num_train_epochs=16,
 		weight_decay=0,
-		#evaluation_strategy="epoch",
 		save_strategy="no",
-		#load_best_model_at_end=True,
 	)
 	
 	trainer = Trainer(
 		model=model,
 		args=training_args,
 		train_dataset=dataset['train'],
-		#eval_dataset=val_dataset,
 		tokenizer=tokenizer,
 		data_collator=data_collator
 	)
 	
-	print("\n\nTraining:\n\n")
-	trainer.train()
-		
-	print()
-	print("Done.")
-
+	trainer.train()	
+	
 if __name__ == '__main__':
 	main()
